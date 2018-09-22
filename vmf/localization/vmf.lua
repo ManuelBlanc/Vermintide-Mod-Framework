bdb182609c19073b216c3c183f6a949ad77efb7d
ManuelBlanc <manuel.blanc@estudiante.uam.es>
2018-09-22 13:59:39 +0200

    Add spanish localization

--- vmf/localization/vmf.lua
+++ vmf/localization/vmf.lua
@@ -1,146 +1,184 @@
 return {
   mods_options = {
     en = "Mod Options",
+    es = "Opciones de los mods",
     ru = "Настройки модов",
   },
   open_vmf_options = {
     en = "Open Options Menu",
+    en = "Abrir el menu de opciones",
     ru = "Открыть меню настроек",
   },
   open_vmf_options_tooltip = {
     en = "Keybind for opening and closing mods options menu.",
+    es = "Atajo para abrir y cerrar el menu de opciones de mods.",
     ru = "Клавиша / сочетание клавиш для открытия и закрытия меню настроек модов.",
   },
   vmf_options_scrolling_speed = {
     en = "Options Menu Scrolling Speed",
+    es = "Velocidad de desplazamiento en el menu",
     ru = "Скорость прокрутки меню",
   },
   ui_scaling = {
     en = "UI Scaling for FHD+ Resolutions",
+    es = "Reescalado de la interfaz para resoluciones Full HD+",
     ru = "Нормализация масштаба UI для FHD+ разрешений",
   },
   ui_scaling_tooltip = {
     en = "Automatically scale UI when resolution exceeds 1080p.",
+    en = "Automaticamente redimensionar la interfaz cuando la resolución exceda 1080p.",
     ru = "Нормализует масштаб элементов интерфейса, если разрешений экрана превышает 1080p.",
   },
   developer_mode = {
     en = "Developer Mode",
+    en = "Modo de desarrollador",
     ru = "Режим разработчика",
   },
   developer_mode_tooltip = {
     en = "Allows you to reload VMF and mods, gives you access to some debug features.",
+    es = "Permite recargar VMF y los mods, y da acceso a herramientas de desarrollo.",
     ru = "Позволяет перезагружать VMF и моды, даёт доступ к инструментам отладки.",
   },
   show_developer_console = {
     en = "Show Developer Console",
+    es = "Mostrar Consola de Desarrollo",
     ru = "Консоль разработчика",
   },
   show_developer_console_tooltip = {
     en = "Opens up the new window showing game log in real time.",
+    es = "",
     ru = "Открывает новое окно, в которое в реальном времени выводится игровой лог.",
   },
   toggle_developer_console = {
     en = "Toggle Developer Console",
+    es = "Abrir Consola de Desarrollo",
     ru = "Открыть/закрыть консоль разработчика",
   },
   show_network_debug_info = {
     en = "Log Network Calls",
+    en = "Depurar las llamadas de red.",
     ru = "Логирование сетевых вызовов",
   },
   show_network_debug_info_tooltip = {
     en = "Log all the VMF network calls and all the data transfered with them.\n\n" ..
          "The method 'info' is used for the logging.",
+    es = "Registra todas las llamadas de red y datos transferidos a través de VMF.\n\n" ..
+         "Esta información se registra a nivel 'info'.",
     ru = "Логирование всех сетевых вызовов VMF и передаваемых с ними данных.\n\n" ..
          "Для логирования используется метод 'info'.",
   },
   log_ui_renderers_info = {
     en = "Log UI Renderers Creation Info",
+    es = "Registra Información de la Renderización de la Interfaz",
     ru = "Логирование информации при создании UI Renderer",
   },
   log_ui_renderers_info_tooltip = {
-    en = "Log the UI Renderer's creator name and all the materials passed as the agruments.\n\n" ..
+    en = "Log the UI Renderer's creator name and all the materials passed as the arguments.\n\n" ..
          "The method 'info' is used for the logging.",
+    es = "Registra el nombre del creador de la interfaz y de todos los materiales recibidos como argumentos\n\n" ..
+         "Esta información se registra a nivel 'info'.",
     ru = "Логирование имени создателя UI Renderer'а и всех материалов, переданных в качестве аргументов.\n\n" ..
          "Для логирования используется метод 'info'.",
   },
   logging_mode = {
     en = "Logging Settings",
+    es = "Opciones de logging",
     ru = "Настройки логирования",
   },
   settings_default = {
     en = "Default",
+    es = "Valor por defecto",
     ru = "Стандартные",
   },
   settings_custom = {
     en = "Custom",
+    es = "Personalizado",
     ru = "Пользовательские",
   },
   output_mode_echo = {
     en = "'Echo' Output",
+    en = "Mensajes de 'Echo'",
     ru = "Вывод 'Echo'",
   },
   output_mode_error = {
     en = "'Error' Output",
+    es = "Mensajes de 'Error'",
     ru = "Вывод 'Error'",
   },
   output_mode_warning = {
     en = "'Warning' Output",
+    es = "Mensajes de 'Warning'",
     ru = "Вывод 'Warning'",
   },
   output_mode_info = {
     en = "'Info' Output",
+    es = "Mensajes de 'Info'",
     ru = "Вывод 'Info'",
   },
   output_mode_debug = {
     en = "'Debug' Output",
+    es = "Mensajes de 'Debug'",
     ru = "Вывод 'Debug'",
   },
   output_disabled = {
     en = "Disabled",
+    es = "Desactivado",
     ru = "Выключен",
   },
   output_log = {
     en = "Log",
+    es = "Registro",
     ru = "Лог",
   },
   output_chat = {
     en = "Chat",
+    es = "Chat",
     ru = "Чат",
   },
   output_log_and_chat = {
     en = "Log & Chat",
+    es = "Registro y chat",
     ru = "Лог и чат",
   },
   chat_history_enable = {
     en = "Chat Input History",
+    es = "Historial de Chat",
     ru = "История ввода чата",
   },
   chat_history_enable_tooltip = {
     en = "Saves all the messages and commands you typed in the chat window.\n\n" ..
          "You can browse your input history by opening the chat and pressing \"Arrow Up\" and \"Arrow Down\".",
+    es = "Guarda todos los mensajes y comandos que escribas en la ventana de chat.\n\n" ..
+         "Puedes navegar por tu historial de comandos abriendo el chat y usando las flechas del teclado.",
     ru = "Сохраняет все сообщения и команды, введённые в чате.\n\n" ..
          "Чтобы пролистывать историю ввода, откройте чат и используйте клавиши \"стрелка вверх\" и \"стрелка вниз\".",
   },
   chat_history_save = {
     en = "Save Input History Between Game Sessions",
+    es = "Guardar la entrada",
     ru = "Сохранять историю ввода между сеансами игры",
   },
   chat_history_save_tooltip = {
     en = "Your chat input history will be saved even after reloading your game (or just VMF).",
+    es = "El texto que introduzcas en el chat se guardara incluso al recargar el juego (o solo VMF)",
     ru = "Когда игрок выключает игру (или перезагружает VMF), VMF cохраняет историю ввода в файл настроек, чтобы загрузить её при следующем запуске игры.",
   },
   chat_history_buffer_size = {
     en = "Input History Buffer Size",
+    es = "Número de comandos antiguos guardados",
     ru = "Размер буфера истории ввода",
   },
   chat_history_buffer_size_tooltip = {
     en = "Maximum number of saved entries.\n\n" ..
          "WARNING: Changing this setting will erase your chat history.",
+    es = "Máximo número de comandos antiguos guardados.\n\n" ..
+         "ATENCIÓN: Cambiar esta preferencia borra el historial del chat.",
     ru = "Максимальное количество сохраняемых записей.\n\n" ..
          "ВНИМАНИЕ: изменение этой настройки очистит вашу историю ввода.",
   },
   chat_history_remove_dups = {
     en = "Remove Duplicate Entries",
+    es = "Borrar Entradas Duplicadas",
     ru = "Удалять повторяющиеся записи",
   },
   chat_history_remove_dups_mode = {
@@ -151,25 +189,32 @@ return {
     en = "Which duplicate entries should be removed.\n\n" ..
          "-- LAST --\nRemoves previous entry if it matches the last one.\n\n" ..
          "-- ALL --\nRemoves all entries if it matches the last one.",
+    es = "Que entradas duplicadas deberian ser borradas.\n\n" ..
+         "-- ÚLTIMA --\nBorra las ",
     ru = "Повторяющиеся записи, которые будут удалены.\n\n" ..
          "-- ПОСЛЕДНИЕ --\nПредпоследняя запись будет удалена, если она совпадает с последней.\n\n" ..
          "-- ВСЕ --\nВсе записи, совпадающие с последней записью, будут удалены.",
   },
   settings_last = {
     en = "Last",
+    es = "Última",
     ru = "Последние",
   },
   settings_all = {
     en = "All",
+    es = "Todo",
     ru = "Все",
   },
   chat_history_commands_only = {
     en = "Save only executed commands",
+    es = "Salvar unicamente los comandos ejecutados",
     ru = "Сохранять только выполненные команды",
   },
   chat_history_commands_only_tooltip = {
     en = "Only successfully executed commands will be saved in the chat history.\n\n" ..
          "WARNING: Changing this setting will erase your chat history.",
+    es = "Solo los comandos ejecutados exitosamente serán salvados en el historial.\n\n" ..
+         "ATENCIÓN: Cambiar esta preferencia borra el historial del chat.",
     ru = "Только успешно выполненные команды будут сохранены в истории ввода.\n\n" ..
          "ВНИМАНИЕ: изменение этой настройки очистит вашу историю ввода.",
   },
@@ -177,14 +222,17 @@ return {
 
   clean_chat_history = {
     en = "cleans chat input history",
+    es = "Borra el historial de usuario",
     ru = "очищает историю ввода",
   },
   dev_console_opened = {
     en = "Developer console opened.",
+    es = "Abierto la consola de desarrollo.",
     ru = "Консоль разработчика открыта.",
   },
   dev_console_closed = {
     en = "Developer console closed.",
+    es = "Cerrado la consola de desarrollo.",
     ru = "Консоль разработчика закрыта.",
   },
 
@@ -192,7 +240,8 @@ return {
   -- MUTATORS
 
   mutator_no_description_provided = {
-    en = "No description provided."
+    en = "No description provided.",
+    es = "No se proporcionó una descripción.",
   },
 
   -- Difficulties' names [V1]
@@ -223,74 +272,95 @@ return {
 
   -- Chat messages
   broadcast_enabled_mutators = {
-    en = "ENABLED MUTATORS"
+    en = "ENABLED MUTATORS",
+    es = "MUTACIONES ACTIVADAS",
   },
   broadcast_all_disabled = {
-    en = "ALL MUTATORS DISABLED"
+    en = "ALL MUTATORS DISABLED",
+    es = "TODAS LAS MUTACIONES DESACTIVADAS",
   },
   broadcast_disabled_mutators = {
-    en = "MUTATORS DISABLED"
+    en = "MUTATORS DISABLED",
+    es = "MUTACIONES DESACTIVADAS",
   },
   local_disabled_mutators = {
-    en = "Mutators disabled"
+    en = "Mutators disabled",
+    es = "Mutaciones desactivadas",
   },
   whisper_enabled_mutators = {
-    en = "[Automated message] This lobby has the following mutators active"
+    en = "[Automated message] This lobby has the following mutators active",
+    en = "[Mensaje automático] Esta partida tiene las siguientes mutaciones",
   },
 
   disabled_reason_not_server = {
-    en = "because you're no longer the host"
+    en = "because you're no longer the host",
+    es = "porque ya no eres el anfitrión",
   },
   disabled_reason_difficulty_change = {
-    en = "DUE TO CHANGE IN DIFFICULTY"
+    en = "DUE TO CHANGE IN DIFFICULTY",
+    es = "DEBIDO A UN CAMBIO DE DIFICULTAD",
   },
 
   -- Interface
   mutators_title = {
-    en = "Mutators"
+    en = "Mutators",
+    es = "Mutaciones",
   },
   mutators_banner_tooltip = {
-    en = "Enable and disable mutators"
+    en = "Enable and disable mutators",
+    es = "Activar y desactivar mutaciones",
   },
   no_mutators = {
-    en = "No mutators installed"
+    en = "No mutators installed",
+    es = "No hay mutaciones instaladas",
   },
   no_mutators_tooltip = {
-    en = "Subscribe to mods and mutators on the workshop"
+    en = "Subscribe to mods and mutators on the workshop",
+    es = "Suscríbete a mods y mutaciones en el workshop",
   },
 
   -- Mutator widgets' tooltips
   tooltip_incompatible_mutators = {
-    en = "\n\n-- INCOMPATIBLE WITH MUTATORS --\n"
+    en = "\n\n-- INCOMPATIBLE WITH MUTATORS --\n",
+    es = "\n\n-- INCOMPATIBLE CON LAS MUTACIONES --\n",
   },
   tooltip_compatible_mutators = {
-    en = "\n\n-- COMPATIBLE ONLY WITH MUTATORS --\n"
+    en = "\n\n-- COMPATIBLE ONLY WITH MUTATORS --\n",
+    es = "\n\n-- COMPATIBLE CON LAS MUTACIONES --\n",
   },
   tooltip_compatible_with_all_mutators = {
-    en = "\n\n-- COMPATIBLE WITH ALL MUTATORS --"
+    en = "\n\n-- COMPATIBLE WITH ALL MUTATORS --",
+    es = "\n\n-- COMPATIBLE CON TODAS LAS MUTACIONES --",
   },
   tooltip_incompatible_with_all_mutators = {
-    en = "\n\n-- INCOMPATIBLE WITH ALL MUTATORS --"
+    en = "\n\n-- INCOMPATIBLE WITH ALL MUTATORS --",
+    es = "\n\n-- INCOMPATIBLE CON TODAS LAS MUTACIONES --",
   },
 
   tooltip_incompatible_diffs = {
-    en = "\n\n-- INCOMPATIBLE WITH DIFFICULTIES --\n"
+    en = "\n\n-- INCOMPATIBLE WITH DIFFICULTIES --\n",
+    es = "\n\n-- INCOMPATIBLE CON LAS DIFICULTADES --\n",
   },
   tooltip_compatible_diffs = {
-    en = "\n\n-- COMPATIBLE ONLY WITH DIFFICULTIES --\n"
+    en = "\n\n-- COMPATIBLE ONLY WITH DIFFICULTIES --\n",
+    es = "\n\n-- COMPATIBLE CON LAS DIFICULTADES --\n",
   },
   tooltip_compatible_with_all_diffs = {
-    en = "\n\n-- COMPATIBLE WITH ALL DIFFICULTIES --"
+    en = "\n\n-- COMPATIBLE WITH ALL DIFFICULTIES --",
+    es = "\n\n-- COMPATIBLE CON TODAS LAS DIFICULTADES --",
   },
 
   tooltip_conflicts = {
-    en = "\n\n-- CONFLICTS --\n"
+    en = "\n\n-- CONFLICTS --\n",
+    es = "\n\n-- CONFLICTOS --\n",
   },
 
   tooltip_append_mutator = {
-    en = " (mutator)"
+    en = " (mutator)",
+    es = " (mutacion)",
   },
   tooltip_append_difficulty = {
-    en = " (difficulty)"
+    en = " (difficulty)",
+    es = " (dificultad)",
   },
 }
